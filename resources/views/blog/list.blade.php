90c2f441fea9f669799ce20d07b1a303ad47e048
setsu556 <qq5y5bsd@gmail.com>
2025-08-11 23:25:51 +0900

    ブログ一覧画面のビューをBladeテンプレートにリファクタリングし、レイアウトとコンポーネントを分離

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,28 +1,6 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>ブログ</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">ブログ</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">ブログ一覧 <span class="sr-only"></span></a>
-                <a class="nav-item nav-link" href="#">ブログ投稿</a>
-            </div>
-        </div>
-    </nav>
-</header>
-<br>
-<div class="container">
+@extends('layout')
+@section('title', 'ブログ一覧')
+@section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h2>ブログ記事一覧</h2>
@@ -42,11 +20,4 @@
             </table>
         </div>
     </div>
-</div>
-<footer class="footer bg-dark  fixed-bottom">
-    <div class="container text-center">
-        <span class="text-light">©︎福のプログラミング講座</span>
-    </div>
-</footer>
-</body>
-</html>
+@endsection
